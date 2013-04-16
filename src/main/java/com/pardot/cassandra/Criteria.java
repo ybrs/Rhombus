0b1304a6269266551e026306c19be7ece0c19b35
Michael Frank <michael.frank@pardot.com>
2013-04-16 14:49:03 -0400

    Create and get by id in ObjectMapper and tests

---
+++ src/main/java/com/pardot/cassandra/Criteria.java
@@ -0,0 +1,11 @@
+package com.pardot.cassandra;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelfrank
+ * Date: 4/16/13
+ * Time: 12:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Criteria {
+}
