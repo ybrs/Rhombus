a9ea8fff806c0f2817a02b9789bd8614045f12c5
Michael Frank <michael.frank@pardot.com>
2013-04-17 10:07:23 -0400

    Initial service setup

---
+++ src/main/java/com/pardot/service/analytics/AnalyticsService.java
@@ -0,0 +1,11 @@
+package com.pardot.service.analytics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelfrank
+ * Date: 4/16/13
+ * Time: 8:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AnalyticsService {
+}
---
+++ src/main/java/com/pardot/service/analytics/ServiceHealthCheck.java
@@ -0,0 +1,9 @@
+package com.pardot.service.analytics;
+
+/**
+ * Pardot, an ExactTarget company
+ * User: Michael Frank
+ * Date: 4/17/13
+ */
+public class ServiceHealthCheck {
+}
