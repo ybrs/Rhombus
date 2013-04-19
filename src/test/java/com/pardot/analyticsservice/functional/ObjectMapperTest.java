601961d0d9cea59214ad24db42fc4342740fd3b6
Michael Frank <michael.frank@pardot.com>
2013-04-19 12:53:14 -0400

    Update and associated functional tests

--- src/test/java/com/pardot/analyticsservice/functional/ObjectMapperTest.java
+++ src/test/java/com/pardot/analyticsservice/functional/ObjectMapperTest.java
@@ -7,6 +7,7 @@ import com.pardot.analyticsservice.cassandra.ObjectMapper;
 import com.pardot.analyticsservice.helpers.TestHelpers;
 import com.pardot.analyticsservice.cassandra.cobject.CKeyspaceDefinition;
 import com.pardot.analyticsservice.cassandra.cobject.CQLGenerationException;
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.TestCase.*;
 
 public class ObjectMapperTest {
@@ -47,7 +49,7 @@ public class ObjectMapperTest {
 		assertEquals(testObject, dbObject);
 
 		//Add another object with the same foreign key
-		om.insert("testtype", TestHelpers.getTestObject(1));
+		UUID key2 = om.insert("testtype", TestHelpers.getTestObject(1));
 
 		//Query by foreign key
 		Criteria criteria = TestHelpers.getTestCriteria(0);
@@ -60,6 +62,24 @@ public class ObjectMapperTest {
 		//Re-query by foreign key
 		dbObjects = om.list("testtype", criteria);
 		assertEquals(1, dbObjects.size());
+
+		//Update the values of one of the objects
+		Map<String, String> testObject2 = TestHelpers.getTestObject(2);
+		UUID key3 = om.update("testtype", key2, testObject2);
+
+		//Get the updated object back and make sure it matches
+		Map<String, String> dbObject2 = om.getByKey("testtype", key3);
+		assertEquals(testObject2, dbObject2);
+
+		//Get from the original index
+		dbObjects = om.list("testtype", criteria);
+		assertEquals(0, dbObjects.size());
+
+		//Get from the new index
+		Criteria criteria2 = TestHelpers.getTestCriteria(1);
+		dbObjects = om.list("testtype", criteria2);
+		assertEquals(1, dbObjects.size());
+
 
 		//Teardown connections
 		cm.teardown();
--- src/test/resources/TestObjects.js
+++ src/test/resources/TestObjects.js
@@ -16,5 +16,14 @@
     "data1": "Second 777 object",
     "data2": "This is data two",
     "data3": "This is data three"
-}
+},
+ {
+     "type": "5",
+     "instance": "333333",
+     "filtered": "1",
+     "foreignid": "778",
+     "data1": "Now a 778 object",
+     "data2": "This is data two",
+     "data3": "This is data three"
+ }
 ]
