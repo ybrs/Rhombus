27fb4ad402e446e2f044d328052199cb55581676
Michael Frank <michael.frank@pardot.com>
2013-06-14 16:57:35 -0400

    Unfinished type coersion from jackson natural types to proper cassandra types.

--- src/main/java/com/pardot/rhombus/util/JsonUtil.java
+++ src/main/java/com/pardot/rhombus/util/JsonUtil.java
@@ -1,10 +1,16 @@
 package com.pardot.rhombus.util;
 
+import com.datastax.driver.core.Row;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
+import com.pardot.rhombus.cobject.CDefinition;
+import com.pardot.rhombus.cobject.CField;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -26,9 +32,85 @@ public class JsonUtil {
 	public static List<Map<String, Object>> rhombusMapFromResource(ClassLoader resourceClassLoader, String resourceLocation) throws IOException {
 		ObjectMapper om = new ObjectMapper();
 		InputStream inputStream = resourceClassLoader.getResourceAsStream(resourceLocation);
-		//System.out.println(inputStream.toString());
 		MapContainer mc = om.readValue(inputStream, MapContainer.class);
 		inputStream.close();
 		return mc.getValues();
 	}
+
+	public static Map<String, Object> rhombusMapFromJsonMap(Map<String, Object> jsonMap, CDefinition definition) {
+		Map<String, Object> rhombusMap = Maps.newHashMap();
+		for(CField field : definition.getFields().values()) {
+			Object jsonValue = jsonMap.get(field.getName());
+			if(jsonValue != null) {
+				rhombusMap.put(field.getName(), typedObjectFromValueAndField(jsonValue, field));
+			}
+		}
+
+		return rhombusMap;
+	}
+
+	private static Object typedObjectFromValueAndField(Object jsonValue, CField field) {
+		if(jsonValue == null) {
+			return null;
+		}
+		Object fieldValue;
+		switch(field.getType()) {
+			case ASCII:
+			case VARCHAR:
+			case TEXT:
+				Preconditions.checkArgument(String.class.isAssignableFrom(jsonValue.getClass()));
+				fieldValue = jsonValue;
+				break;
+			case BIGINT:
+			case COUNTER:
+				fieldValue = longFromNumber(jsonValue);
+				break;
+			case BLOB:
+				throw new IllegalArgumentException();
+			case BOOLEAN:
+				fieldValue = booleanFromNumber(jsonValue);
+				break;
+			case DECIMAL:
+				fieldValue = jsonValue.getDecimal(field.getName());
+				break;
+			case DOUBLE:
+				fieldValue = jsonValue.getDouble(field.getName());
+				break;
+			case FLOAT:
+				fieldValue = jsonValue.getFloat(field.getName());
+				break;
+			case INT:
+				fieldValue = jsonValue.getInt(field.getName());
+				break;
+			case TIMESTAMP:
+				fieldValue = jsonValue.getDate(field.getName());
+				if(fieldValue != null) {
+					fieldValue = ((Date)fieldValue).getTime();
+				}
+				break;
+			case UUID:
+			case TIMEUUID:
+				fieldValue = jsonValue.getUUID(field.getName());
+				break;
+			case VARINT:
+				fieldValue = jsonValue.getVarint(field.getName());
+				break;
+			default:
+				fieldValue = null;
+		}
+		return (fieldValue == null ? null : fieldValue.toString());
+	}
+
+	private static Long longFromNumber(Object number) {
+		if(Boolean.class.isAssignableFrom(number.getClass())) {
+			return ((Boolean)number ? 1L : 0L);
+		} else if(Integer.class.isAssignableFrom(number.getClass())) {
+			return ((Integer)number).longValue();
+		} else if(Long.class.isAssignableFrom(number.getClass())) {
+			return (Long)number;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
 }
