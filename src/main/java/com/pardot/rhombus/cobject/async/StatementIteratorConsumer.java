b2dac1d09239d747e5fa2fe064e756158e6eb6c0
Rob Righter <rob.righter@pardot.com>
2013-07-25 12:40:32 -0400

    Optimizations for async + speed test

--- src/main/java/com/pardot/rhombus/cobject/async/StatementIteratorConsumer.java
+++ src/main/java/com/pardot/rhombus/cobject/async/StatementIteratorConsumer.java
@@ -1,6 +1,7 @@
 package com.pardot.rhombus.cobject.async;
 
 import com.datastax.driver.core.*;
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.pardot.rhombus.cobject.BoundedCQLStatementIterator;
@@ -13,11 +14,9 @@ import com.yammer.metrics.core.TimerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import static com.google.common.util.concurrent.Uninterruptibles.awaitUninterruptibly;
 
@@ -29,50 +28,46 @@ import static com.google.common.util.concurrent.Uninterruptibles.awaitUninterrup
 public class StatementIteratorConsumer {
 
 	private static Logger logger = LoggerFactory.getLogger(StatementIteratorConsumer.class);
-	private static final ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-	public final Timer latencies = Metrics.newTimer(StatementIteratorConsumer.class, "latencies", TimeUnit.MILLISECONDS, TimeUnit.SECONDS);
+	private static ExecutorService executorService = Executors.newFixedThreadPool(400);
 
 	private final BoundedCQLStatementIterator statementIterator;
 	private CQLExecutor cqlExecutor;
 	private final CountDownLatch shutdownLatch;
-	private final long statementTimeout;
+	private final long timeout;
 
-	public StatementIteratorConsumer(BoundedCQLStatementIterator statementIterator, CQLExecutor cqlExecutor, long statementTimeout) {
+	public StatementIteratorConsumer(BoundedCQLStatementIterator statementIterator, CQLExecutor cqlExecutor, long timeout) {
 		this.statementIterator = statementIterator;
 		this.cqlExecutor = cqlExecutor;
-		this.statementTimeout = statementTimeout;
-		shutdownLatch = new CountDownLatch((new Long(statementIterator.size())).intValue());
+		this.timeout = timeout;
+		this.shutdownLatch = new CountDownLatch((new Long(statementIterator.size())).intValue());
 
 	}
 
 	public void start() {
-		executorService.execute(new Runnable() {
-			@Override
-			public void run() {
-				request();
-			}
-		});
-	}
-
-	private void request() {
 		while(statementIterator.hasNext()) {
-			handle(statementIterator.next());
+			final CQLStatement next = statementIterator.next();
+			Runnable r = new Runnable() {
+				@Override
+				public void run() {
+					handle(next);
+				}
+			};
+			executorService.execute(r);
 		}
 	}
 
 	public void join() {
-		logger.debug("awaitUninterruptibly with timeout {}ms", statementTimeout);
-		awaitUninterruptibly(shutdownLatch, statementTimeout, TimeUnit.MILLISECONDS);
+		//logger.debug("awaitUninterruptibly with timeout {}ms", statementTimeout);
+		awaitUninterruptibly(shutdownLatch, timeout, TimeUnit.MILLISECONDS);
 	}
 
 	protected void handle(CQLStatement statement) {
-		final TimerContext timerContext = latencies.time();
 		ResultSetFuture future = this.cqlExecutor.executeAsync(statement);
 		Futures.addCallback(future, new FutureCallback<ResultSet>() {
 			@Override
 			public void onSuccess(final ResultSet result) {
 				Host queriedHost = result.getExecutionInfo().getQueriedHost();
-				logger.debug("queried host: {} in datacenter {}", queriedHost, queriedHost.getDatacenter());
+				//logger.debug("queried host: {} in datacenter {}", queriedHost, queriedHost.getDatacenter());
 				Metrics.defaultRegistry().newMeter(StatementIteratorConsumer.class, "queriedhost." + queriedHost.getDatacenter(), queriedHost.getDatacenter(), TimeUnit.SECONDS).mark();
 				shutdownLatch.countDown();
 			}
@@ -82,6 +77,6 @@ public class StatementIteratorConsumer {
 				logger.error("Error during async request: {}", t);
 				shutdownLatch.countDown();
 			}
-		}, executorService);
+		});
 	}
 }
---
+++ src/test/java/com/pardot/rhombus/functional/AsyncSpeedITCase.java
@@ -0,0 +1,124 @@
+package com.pardot.rhombus.functional;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.pardot.rhombus.ConnectionManager;
+import com.pardot.rhombus.Criteria;
+import com.pardot.rhombus.ObjectMapper;
+import com.pardot.rhombus.UpdateProcessor;
+import com.pardot.rhombus.cobject.CDefinition;
+import com.pardot.rhombus.cobject.CIndex;
+import com.pardot.rhombus.cobject.CKeyspaceDefinition;
+import com.pardot.rhombus.cobject.CQLStatement;
+import com.pardot.rhombus.helpers.TestHelpers;
+import org.joda.time.DateTime;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Pardot, An ExactTarget Company
+ * User: robrighter
+ * Date: 7/25/13
+ */
+public class AsyncSpeedITCase {
+
+	private static Logger logger = LoggerFactory.getLogger(ObjectMapperITCase.class);
+
+	@Test
+	public void testAsyncSpeed() throws Exception {
+		logger.debug("Starting testObjectMapper");
+
+		//Build the connection manager
+		ConnectionManager cm = getConnectionManager();
+
+		//Build our keyspace definition object
+		String json = TestHelpers.readFileToString(this.getClass(), "CKeyspaceTestData.js");
+		CKeyspaceDefinition definition = CKeyspaceDefinition.fromJsonString(json);
+		assertNotNull(definition);
+
+		//Rebuild the keyspace and get the object mapper
+		cm.buildKeyspace(definition, true);
+		cm.setDefaultKeyspace(definition);
+		ObjectMapper om = cm.getObjectMapper();
+		CDefinition def1 = om.getKeyspaceDefinition_ONLY_FOR_TESTING().getDefinitions().get("testtype");
+
+
+		//do an insert on an object
+		//make some objects to insert
+		List<Map<String,Object>> toInserts = Lists.newArrayList();
+		int numberOfObjects = 9000;
+		for(int i = 0; i < numberOfObjects; i++){
+			Map<String, Object> testObject = Maps.newTreeMap();
+			testObject.put("foreignid", Long.valueOf(i%100));
+			testObject.put("type", Integer.valueOf(i%5));
+			testObject.put("instance", Long.valueOf(i%10));
+			testObject.put("filtered", Integer.valueOf(1));
+			testObject.put("data1", "This is data 1 "+i);
+			testObject.put("data2", "This is data 2 "+i);
+			testObject.put("data3", "This is data 3 "+i);
+			toInserts.add(testObject);
+		}
+
+		Map<String,List<Map<String,Object>>> objects = Maps.newHashMap();
+		objects.put("testtype", toInserts);
+
+		long start = 0;
+		long end = 0;
+		long asyncTime = 0;
+		long syncTime = 0;
+
+		//insert sync
+		om.setExecuteAsync(false);
+		start = System.currentTimeMillis();
+		om.insertBatchMixed(objects);
+		end = System.currentTimeMillis();
+		syncTime = end - start;
+
+		//Rebuild the keyspace and get the object mapper
+		cm = getConnectionManager();
+		cm.buildKeyspace(definition, true);
+		cm.setDefaultKeyspace(definition);
+		om = cm.getObjectMapper();
+		def1 = om.getKeyspaceDefinition_ONLY_FOR_TESTING().getDefinitions().get("testtype");
+
+
+		//insert async
+		om.setExecuteAsync(true);
+		start = System.currentTimeMillis();
+		om.insertBatchMixed(objects);
+		end = System.currentTimeMillis();
+		asyncTime = end - start;
+
+
+
+
+		logger.info("Insert Speed Results");
+		logger.info("======================");
+		logger.info("Sync Time ms: " + syncTime);
+		logger.info("Async Time ms: " + asyncTime);
+
+		assertTrue((asyncTime*2) < syncTime);
+
+	}
+
+
+	private ConnectionManager getConnectionManager() throws IOException {
+		//Get a connection manager based on the test properties
+		ConnectionManager cm = TestHelpers.getTestConnectionManager();
+		cm.setLogCql(false);
+		cm.buildCluster();
+		assertNotNull(cm);
+		return cm;
+	}
+}
