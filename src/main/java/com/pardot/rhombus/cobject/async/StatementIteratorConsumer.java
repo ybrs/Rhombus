5a81c999a12e6918843da35f7d35b1cf1e676806
Rob Righter <rob.righter@pardot.com>
2013-07-25 00:12:03 -0400

    fixed consumer removal

--- src/main/java/com/pardot/rhombus/cobject/async/StatementIteratorConsumer.java
+++ src/main/java/com/pardot/rhombus/cobject/async/StatementIteratorConsumer.java
@@ -1,7 +1,6 @@
 package com.pardot.rhombus.cobject.async;
 
 import com.datastax.driver.core.*;
-import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.pardot.rhombus.cobject.BoundedCQLStatementIterator;
@@ -14,7 +13,6 @@ import com.yammer.metrics.core.TimerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -38,7 +36,6 @@ public class StatementIteratorConsumer {
 	private CQLExecutor cqlExecutor;
 	private final CountDownLatch shutdownLatch;
 	private final long statementTimeout;
-	private List<StatementConsumer> consumers;
 
 	public StatementIteratorConsumer(BoundedCQLStatementIterator statementIterator, CQLExecutor cqlExecutor, long statementTimeout) {
 		this.statementIterator = statementIterator;
@@ -58,16 +55,33 @@ public class StatementIteratorConsumer {
 	}
 
 	private void request() {
-		consumers = Lists.newArrayList();
 		while(statementIterator.hasNext()) {
-			StatementConsumer consumer = new StatementConsumer(statementIterator.next(), cqlExecutor, statementTimeout);
-			consumers.add(consumer);
+			handle(statementIterator.next());
 		}
 	}
 
 	public void join() {
-		for(StatementConsumer consumer : consumers) {
-			consumer.join();
-		}
+		logger.debug("awaitUninterruptibly with timeout {}ms", statementTimeout);
+		awaitUninterruptibly(shutdownLatch, statementTimeout, TimeUnit.MILLISECONDS);
+	}
+
+	protected void handle(CQLStatement statement) {
+		final TimerContext timerContext = latencies.time();
+		ResultSetFuture future = this.cqlExecutor.executeAsync(statement);
+		Futures.addCallback(future, new FutureCallback<ResultSet>() {
+			@Override
+			public void onSuccess(final ResultSet result) {
+				Host queriedHost = result.getExecutionInfo().getQueriedHost();
+				logger.debug("queried host: {} in datacenter {}", queriedHost, queriedHost.getDatacenter());
+				Metrics.defaultRegistry().newMeter(StatementIteratorConsumer.class, "queriedhost." + queriedHost.getDatacenter(), queriedHost.getDatacenter(), TimeUnit.SECONDS).mark();
+				shutdownLatch.countDown();
+			}
+			@Override
+			public void onFailure(final Throwable t) {
+				//TODO Stop processing and return error
+				logger.error("Error during async request: {}", t);
+				shutdownLatch.countDown();
+			}
+		}, executorService);
 	}
 }
